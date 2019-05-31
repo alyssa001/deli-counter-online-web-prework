3e55feca6ac718de4a4062f13e47b5403dcffb28
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-31 01:10:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,11 @@ def line(array)
   if array.count == 0 
     puts "The line is currently empty."
   else 
+    response = "The line is currently: "
     array.each_with_index do |name, index|
-    response += "Welcome, #{name}. You are number #{index+1}. "
+    response += "#{index+1}. #{name} "
   end
   puts response
   end
 end
+
