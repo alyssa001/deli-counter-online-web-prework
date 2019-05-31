8829ab6d7bbcdf3a8fb814cb62cedc2c1f8e44b9
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-31 01:04:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,15 @@
 
 katz_deli = [ ]
 def line(queue)
-  
-def line(array)
+  if array.count == 0 
+    puts "The line is currently empty."
+  else 
+    array.each_with_index do |name, index|
+    response += "Welcome, #{name}. You are number #{index+1}. "
+  end
+  puts response
+  end
+def ew(array)
   if array.count == 0 
     puts "The line is currently empty."
   else 
